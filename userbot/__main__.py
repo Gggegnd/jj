3c16f239f55e37de1b239bdb192015fadf54e152
abd200580 <98943893+alhufra@users.noreply.github.com>
2022-02-03 16:48:15 +0300

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,11 +1,3 @@
-# @Jmthon - < https://t.me/Jmthon >
-# Copyright (C) 2021 - JMTHON-AR
-# All rights reserved.
-#
-# This file is a part of < https://github.com/JMTHON-AR/JMTHON >
-# Please read the GNU Affero General Public License in;
-# < https://github.com/JMTHON-AR/JM-THON/blob/master/LICENSE
-# ===============================================================
 import sys
 import os
 import re
@@ -34,9 +26,9 @@ print("جميع الحقوق والملفات محفوظة " + userbot.__license
 cmdhr = Config.COMMAND_HAND_LER
 
 try:
-    LOGS.info(f"⚒️ يتم تشغيل جمثون")
+    LOGS.info(f"⚒️ يتم تشغيل الحفرة")
     jmthon.loop.run_until_complete(setup_bot())
-    LOGS.info(f"✅ انتهاء التشغيل ")
+    LOGS.info(f"✔️ انتهاء التشغيل ")
 except Exception as e:
     LOGS.error(f"{str(e)}")
     sys.exit()
@@ -58,7 +50,7 @@ async def startup_process():
     await load_plugins("plugins")
     await load_plugins("assistant")
     print("----------------------------------")
-    print("تم بنجاح اكتمال تنصيب سورس جمثون المجاني ✓")
+    print("تم بنجاح اكتمال تنصيب سورس الحفرة المجاني ✓")
     print(
         " - ارسل  .فحص  للتأكد من البوت\n-  ولعرض اوامر السورس ارسل  .الاوامر\n-  للمزيد من المعلومات ادخل الى مجموعتك في التليجرام"
     )
