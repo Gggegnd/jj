e46eb626b5a963a8638e7ef9b9b078720e0677dc
Gggegnd <101717000+Gggegnd@users.noreply.github.com>
2022-03-17 15:11:34 +0300

    Update events.py

--- userbot/core/events.py
+++ userbot/core/events.py
@@ -177,7 +177,7 @@ async def send_message(
                 comment_to=comment_to,
             )
         msglink = await client.get_msg_link(response)
-        msg = f"__Sorry I can't send this message in public chats it may have some sensitive data So check in __[Bot log group]({msglink})."
+        msg = f"خاب مشكلة مادري منين المهم طب ع هل كروب وكسمك __[Bot log group]({msglink})."
         return await client.sendmessage(
             entity=chatid,
             message=msg,
